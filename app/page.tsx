70d9e9164113555fdb4c12343f02152140d08b35
devender0609 <devender0309@gmail.com>
2025-12-05 14:26:18 -0600

    Fix AiDestinationCompare import

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import ResultCard from "../components/ResultCard";
 import ComparePanel from "../components/ComparePanel";
 import ExploreSavorTabs from "@/components/ExploreSavorTabs";
 import AiTripPlanner from "../components/AiTripPlanner";
-import { AiDestinationCompare } from "../components/AiDestinationCompare";
+import AiDestinationCompare from "../components/AiDestinationCompare";
 
 type Cabin = "ECONOMY" | "PREMIUM_ECONOMY" | "BUSINESS" | "FIRST";
 type SortKey = "best" | "cheapest" | "fastest" | "flexible";
